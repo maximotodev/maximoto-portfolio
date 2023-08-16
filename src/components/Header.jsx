2f5cdd6064350bc5161a4a52eb15fd12f3aac7f8
maximotodev <maximotodev@proton.me>
2023-08-16 02:14:46 -0400

    added useRef to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { React, useState} from "react"
+import { React, useEffect, useRef, useState} from "react"
 import { Link, NavLink } from 'react-router-dom'
 import './Header.css'
 import logoImg from '../assets/company-logo.png'
@@ -7,6 +7,25 @@ import Hamburger from 'hamburger-react'
 const Header = () => {
 
   const [isOpen, setOpen] = useState(false)
+
+  let navbarRef = useRef();
+
+  useEffect(()=>{
+    
+    let handler = (e) => {
+      if(!navbarRef.current.contains(e.target)) {
+        setOpen(false);
+        console.log(navbarRef.current)
+      }
+    };
+
+    document.addEventListener('mousedown', handler);
+
+    return() => {
+      document.removeEventListener('mousedown', handler);
+    }
+
+  })
 
   const activeStyle = {
     fontWeight: "bold",
@@ -24,13 +43,16 @@ const Header = () => {
       <Link
         className="logo-wrapper"
         to='/'>
-      <img className="img-logo" src={logoImg} alt="logo" />
-      <h1 className="company-name">maximoto</h1>
+        <img className="img-logo" src={logoImg} alt="logo" />
+        <h1 className="company-name">maximoto</h1>
       </Link>
-      <Hamburger direction='right' color="#ff5722" toggled={isOpen} toggle={setOpen} />
-      <nav 
+      <div className="hamburger">
+        <Hamburger size={25} easing="ease-in" color="#ff5722" toggled={isOpen} toggle={setOpen} />
+      </div>
+      <nav
+        ref={navbarRef} 
         className="navbar"
-        style={isOpen ? navbarStyle : null }>
+        style={isOpen ? navbarStyle : {display: 'none'} }>
         <NavLink
             className='navlink'
             to='/'
@@ -44,7 +66,9 @@ const Header = () => {
         <NavLink
             className='navlink'
             to="blog"
-            style={({isActive}) => isActive ? activeStyle : null}>blog
+            style={({isActive}) => isActive ? activeStyle : null}
+            >
+              blog
         </NavLink>
         <NavLink
             className='navlink'
